ede4142be1356a33b23bf4ad26911a1286996dd8
netmanchris <chris_young@me.com>
2019-06-27 11:20:43 -0400

    Fixing tests after breaking changes

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         # Versions should comply with PEP440.  For a discussion on single-sourcing
         # the version across test_machine.py and the project code, see
         # https://packaging.python.org/en/latest/single_source_version.html
-        version='0.1.3',
+        version='0.1.3ss',
 
         description='Python binding for HPE Composable Fabric Manager',
         long_description=long_description,
