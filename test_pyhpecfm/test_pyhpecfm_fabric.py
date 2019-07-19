5e7b44fbcd35a30ebda62ee2ebd6dd5f41c711ac
netmanchris <chris_young@me.com>
2019-07-19 13:38:00 -0400

    Add vcrpy recording of test API call results in preperation of public CI.

--- test_pyhpecfm/test_pyhpecfm_fabric.py
+++ test_pyhpecfm/test_pyhpecfm_fabric.py
@@ -4,7 +4,7 @@ Module for testing the functions in pyhpecfm.fabric.
 """
 
 import os
-
+import vcr
 from unittest import TestCase
 from unittest import mock
 from nose.plugins.skip import SkipTest
@@ -17,21 +17,28 @@ cfm_username = os.environ['CFM_USERNAME']
 cfm_password = os.environ['CFM_PASSWORD']
 
 cfm = client.CFMClient(cfm_ip, cfm_username,cfm_password)
-cfm.connect()
+#cfm.connect()
 
 #TODO TAKE OUT HARDCODED DATA LATER
-
+my_vcr = vcr.VCR(
+    serializer='json',
+    cassette_library_dir='fixtures/cassettes',
+    record_mode='new_episodes',
+    match_on=['uri', 'method'],
+)
 
 class TestGetSwitches(TestCase):
     """
     Test Case for pyhpecfm.fabric get_switches function
     """
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_switches(self):
         """
         Simple test to return switches. URL has no parameters
         :return:
         """
+        cfm.connect()
         test_switches = fabric.get_switches(cfm)
         my_attributes = ['segment','fabric_uuid', 'fitting_number', 'ip_gateway', 'hostip_state', 'ip_address_v6', 'uuid', 'ip_mode', 'ip_gateway_v6', 'health', 'mac_address', 'ip_mode_v6', 'serial_number', 'status', 'description', 'ip_address', 'model', 'hw_revision', 'sw_version', 'name', 'ip_mask', 'configuration_number', 'operational_stage', 'ip_mask_v6']
         self.assertIs(type(test_switches), list)
@@ -39,10 +46,12 @@ class TestGetSwitches(TestCase):
         for i in test_switches[0].keys():
             self.assertIn(i, my_attributes)
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_switches_single_param(self):
         """
         Test to return switches. Request is launched with a single parameter of ports.
         """
+        cfm.connect()
         test_switches = fabric.get_switches(cfm, params={'ports': True})
         my_attributes = ['ports','segment','fabric_uuid', 'fitting_number', 'ip_gateway',
                          'hostip_state', 'ip_address_v6', 'uuid', 'ip_mode', 'ip_gateway_v6', 'health', 'mac_address', 'ip_mode_v6', 'serial_number', 'status', 'description', 'ip_address', 'model', 'hw_revision', 'sw_version', 'name', 'ip_mask', 'configuration_number', 'operational_stage', 'ip_mask_v6']
@@ -51,11 +60,13 @@ class TestGetSwitches(TestCase):
         for i in test_switches[0].keys():
             self.assertIn(i, my_attributes)
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_switches_multiple_param(self):
         """
         Test to return switches. Request is launched with multiple parameters of Ports = True and a specific
         fabric on my CFM system.
         """
+        cfm.connect()
         test_switches = fabric.get_switches(cfm)
         cfm_fabric = test_switches[0]['fabric_uuid']
         test_switches = fabric.get_switches(cfm, params={'ports': True, 'fabric' : cfm_fabric})
@@ -70,9 +81,12 @@ class TestGetPorts(TestCase):
     """
     Test case for pyhpecfm.fabric get_ports function
     """
+
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_ports(self):
         """
         """
+        cfm.connect()
         test_switches = fabric.get_switches(cfm)
         test_switch = test_switches[0]['uuid']
         ports_list = fabric.get_ports(cfm, test_switch)
@@ -94,10 +108,12 @@ class TestGetFabric(TestCase):
     Test
     case for pyhpecfm.fabric get_fabric function
     """
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_fabric(self):
         """
         General test for get_fabric function
         """
+        cfm.connect()
         test_fabric = fabric.get_fabrics(cfm)
         my_attributes = ['description', 'foreign_manager_id', 'foreign_fabric_state', 'name',
                          'is_stable', 'foreign_management_state', 'foreign_manager_url', 'uuid']
@@ -106,11 +122,13 @@ class TestGetFabric(TestCase):
         for i in test_fabric[0].keys():
             self.assertIn(i, my_attributes)
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_specific_fabric(self):
         """
         Test for get_fabrics using specific UUID
         :return:
         """
+        cfm.connect()
         all_fabrics = fabric.get_fabrics(cfm)
         my_fabric = all_fabrics[0]['uuid']
         test_fabric = fabric.get_fabrics(cfm, fabric_uuid=my_fabric)
@@ -127,10 +145,13 @@ class TestGetFabric_IP_Networks(TestCase):
     Test
     case for pyhpecfm.fabric get_fabric function
     """
+
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_fabric_ip_networks(self):
         """
         General test for pyhpecfm.fabric.get_fabric_ip_networks function
         """
+        cfm.connect()
         test_fabric = fabric.get_fabric_ip_networks(cfm)
         my_attributes = ['subnet', 'fabric_uuid', 'name', 'switch_addresses', 'vlan', 'uuid',
                          'mode', 'description']
@@ -144,10 +165,13 @@ class TestGetVLANGroups(TestCase):
     Test
     case for pyhpecfm.fabric get_vlan_groups function
     """
+
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_vlan_groups(self):
         """
         General test for pyhpecfm.fabric.get_vlan_groups function
         """
+        cfm.connect()
         test_vlan_groups = fabric.get_vlan_groups(cfm)
         my_attributes = ['lag_uuids', 'description', 'vlans', 'uuid', 'name']
         self.assertIs(type(test_vlan_groups), list)
@@ -155,11 +179,13 @@ class TestGetVLANGroups(TestCase):
         for i in test_vlan_groups[0].keys():
             self.assertIn(i, my_attributes)
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_vlan_groups_with_params(self):
         """
         General test for pyhpecfm.fabric.get_vlan_groups function with parameters to select
         a single VLAN group
         """
+        cfm.connect()
         params = {'name': 'My_New_VLAN_Group'}
         test_vlan_groups = fabric.get_vlan_groups(cfm, params=params)
         my_attributes = ['lag_uuids', 'description', 'vlans', 'uuid', 'name']
@@ -173,10 +199,13 @@ class TestGetVLANProperties(TestCase):
     Test
     case for pyhpecfm.fabric get_vlan_properties function
     """
+
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_vlan_properties(self):
         """
         General test for pyhpecfm.fabric.get_vlan_groups function
         """
+        cfm.connect()
         all_fabrics = fabric.get_fabrics(cfm)
         my_fabric = all_fabrics[0]['uuid']
         test_vlan_properties = fabric.get_vlan_properties(cfm, my_fabric)
--- test_pyhpecfm/test_pyhpecfm_system.py
+++ test_pyhpecfm/test_pyhpecfm_system.py
@@ -4,6 +4,8 @@ Module for testing functions in  pyhpecfm.system.
 """
 
 import os
+import vcr
+import logging
 
 from unittest import TestCase
 from unittest import mock
@@ -16,21 +18,30 @@ cfm_ip= os.environ['CFM_IP']
 cfm_username= os.environ['CFM_USERNAME']
 cfm_password= os.environ['CFM_PASSWORD']
 
+
 cfm = client.CFMClient(cfm_ip, cfm_username,cfm_password)
-cfm.connect()
+#cfm.connect()
 
 #TODO TAKE OUT HARDCODED DATA LATER
 
+
+
+logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from vcrpy
+vcr_log = logging.getLogger("vcr")
+vcr_log.setLevel(logging.INFO)
 
 class TestGetVersions(TestCase):
     """
     Test Case for pyhpecfm.fabric get_switches function
     """
 
+
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_versions(self):
         """
         Test pyhpeimc.system.get_version function.
         """
+        cfm.connect()
         test_version = system.get_versions(cfm)
         my_attributes = ['current', 'supported', 'software']
         self.assertIs(type(test_version), dict)
@@ -42,10 +53,12 @@ class TestGetAuditLogs(TestCase):
     Test Case for pyhpecfm.fabric get_switches function
     """
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_audit_logs(self):
         """
         Test pyhpecfm.system.get_audit_logs function.
         """
+        cfm.connect()
         my_logs = system.get_audit_logs(cfm)
         my_attributes = ['description', 'record_type', 'log_date', 'uuid',
                          'stream_id', 'data', 'severity']
@@ -59,20 +72,24 @@ class TestGetBackups(TestCase):
     Test Cases for pyhpecfm.system get_backups function
     """
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_backups(self):
         """
         Test pyhpecfm.system.get_backups function.
         """
+        cfm.connect()
         my_backups = system.get_backups(cfm)
         my_attributes = ['url', 'checksum', 'uuid', 'name', 'date_created']
         self.assertIs(type(my_backups), list)
         for i in my_backups[0].keys():
             self.assertIn(i, my_attributes)
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_specific_backup(self):
         """
         Test pyhpecfm.system.get_backups function.
         """
+        cfm.connect()
         my_backups = system.get_backups(cfm)
         my_uuid = my_backups[0]['uuid']
         single_backup = system.get_backups(cfm, my_uuid)
@@ -86,10 +103,12 @@ class TestCreateBackup(TestCase):
     Test Case for pyhpecfm.system create_backup function
     """
 
+    @vcr.use_cassette(cassette_library_dir='fixtures/cassettes')
     def test_get_backups(self):
         """
         Test pyhpecfm.system.create_backup function.
         """
+        cfm.connect()
         backup_result = system.create_backup(cfm)
         my_attributes = ['url', 'checksum', 'uuid', 'name', 'date_created']
         self.assertIs(type(backup_result), list)
@@ -97,19 +116,8 @@ class TestCreateBackup(TestCase):
             self.assertIn(i, my_attributes)
 
 
-class TestGetAudit(TestCase):
-    """
-    Test Case for pyhpecfm.system get_audit_logs function
-    """
 
-    def test_get_audit_logs(self):
-        """
-        Test pyhpecfm.system.create_backup function.
-        """
-        audit_logs = system.get_audit_logs(cfm)
-        my_attributes = ['description', 'record_type', 'log_date', 'uuid', 'stream_id', 'data', 'severity']
-        self.assertIs(type(audit_logs), list)
-        for i in audit_logs[0].keys():
-            self.assertIn(i, my_attributes)
+
+
 
 
